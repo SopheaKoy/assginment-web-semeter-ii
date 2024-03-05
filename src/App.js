178b6c86efe5e50870f293fb306b76091bc00690
SopheaKoy <koysophea09@gmail.com>
2024-03-05 22:53:41 +0700

    add on struct

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Layout from './component/Layout';
+import Layout from "./Components/Layout";
 
-import Homepage from './page/Homepage';
-import Categorypage from './page/Categorypage';
-import Aboutpage from './page/Aboutpage';
-
+import Homepage from "./page/Homepage";
+import Categorypage from "./page/category/Categorypage";
+import Aboutpage from "./page/about/Aboutpage";
 
 export default function App() {
   return (
-    <div className="App">
-        <Layout />
-        <BrowserRouter>  
-          <Routes>
-            <Route path="/" element={ <Homepage /> } />
-            <Route path="/category" element={ <Categorypage /> } />
-            <Route path="/about" element={ <Aboutpage /> } />
-            <Route path="*" element={ <h1>Not Found</h1> } />
-          </Routes>
-        </BrowserRouter>
+    <div className='App'>
+      <Layout />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/category' element={<Categorypage />} />
+          <Route path='/about' element={<Aboutpage />} />
+          <Route path='*' element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
