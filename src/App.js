ebce13115da3683a164a5d2889078971a1188819
Meanborey <boreysinmean11@gmail.com>
2024-03-15 00:02:08 +0700

    commit navba and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import Layout from './component/Layout';
-
-import Homepage from './page/Homepage';
 import Categorypage from './page/Categorypage';
 import Aboutpage from './page/Aboutpage';
-
+import Subhomepage from './components/subhomepage';
 
 export default function App() {
   return (
-    <div className="App">
-        <Layout />
-        <BrowserRouter>  
+    <div className="App ">
+      
+      <Subhomepage/>
+
+
+        {/* <BrowserRouter>  
           <Routes>
             <Route path="/" element={ <Homepage /> } />
             <Route path="/category" element={ <Categorypage /> } />
             <Route path="/about" element={ <Aboutpage /> } />
             <Route path="*" element={ <h1>Not Found</h1> } />
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
     </div>
   );
 }
---
+++ src/components/subhomepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export default function subhomepage() {
+  return (
+    <div className='bg-gray-200'>
+       <Navbar/>
+ 
+        <Footer/>
+    </div>
+  )
+}
